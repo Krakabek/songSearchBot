1c8c88b3b5fce15a6ba40cd8522e2ddd6a2633bb
danilradkovsky <dradkovsky@readdle.com>
2018-03-07 00:26:38 +0200

    Fix apple music response handling, Added bot functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-class TsTest {
-    run() {
-        console.log("TEST!!");
-    }
-}
+import {SearchAMusic} from "./apple-music";
 
-const tsTest = new TsTest();
-tsTest.run();
+const Telegraf = require("telegraf");
+const token = "569551045:AAFwKVYioRaTwlHNuaA-qjo7G-uj0ClBzCM";
+
+const bot = new Telegraf(token);
+const commandRegexp = /\/songlink /;
+
+bot.hears(commandRegexp, (ctx: any) => {
+    const songName = ctx.message.text.replace(commandRegexp, "");
+    SearchAMusic(songName).then((result) => {
+        ctx.reply(result + "\n");
+    })
+});
+bot.startPolling();
