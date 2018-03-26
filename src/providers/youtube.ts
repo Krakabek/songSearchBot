9884b9005e6e0c7e80d3057efd2d5a9e45c5dd4a
Alex <alexey.shulev@gmail.com>
2018-03-26 16:39:46 +0300

    applying requested changes

--- src/providers/youtube.ts
+++ src/providers/youtube.ts
@@ -1,16 +1,15 @@
 import * as Bluebird from "bluebird";
 import {ProviderResponse} from "./interfaces";
-import {formatResponse} from "../formatter";
+import {formatQuery, formatResponse} from "../formatter";
 
 export function SearchYoutube(songname: string):  Bluebird<ProviderResponse> {
     return Bluebird.resolve()
         .then(() => {
             const youtubeSearchLink = `
                 https://www.youtube.com/results?
-                search_query=${encodeURI(songname)}
+                search_query=${formatQuery(songname)}
                 &page=&utm_source=opensearch
             `.replace(/[\n\s]/g, "");
-            console.dir(youtubeSearchLink);
             return {
                 url: formatResponse("YouTube", youtubeSearchLink),
                 albumCover: ""
